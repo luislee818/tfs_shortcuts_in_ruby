cabf4b17d1ce8fb465cf20f7a60e880a9269ea50
Dapeng Li <luislee818@gmail.com>
2013-05-12 15:44:40 +0800

    Add logging outpu for custom workflow

--- custom_commands.rb
+++ custom_commands.rb
@@ -1,13 +1,26 @@
 add_command 'workflow_fancy_get_dev' do
 	log '---------- Getting Dev branch --------'
 	get_dev
-	log '---------- Get Dev branch Done --------'
+	log '---------- Get Dev branch done --------'
 end
 
 add_command 'workflow_merge_dev_to_int' do
+	log '---------- Getting Dev branch --------'
 	get_dev
+	log '---------- Get Dev branch done --------'
+
+	log '---------- Getting Integration branch --------'
 	get_integration
+	log '---------- Get Integration branch done --------'
+
+	log '---------- Merging from Dev to Integration branch --------'
 	merge_dev_to_int
+	log '---------- Merge from Dev to Integration branch done --------'
+
+	log '---------- Resolving potential merge conflicts by taking from source --------'
 	resolve
+	log '---------- Resolve potential merge conflicts done --------'
+
+	log '---------- Checking in merge results to Integration without prompt --------'
 	checkin_int_without_prompt
 end
